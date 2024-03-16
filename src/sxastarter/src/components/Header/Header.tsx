5f76c9cadb839c4de16e5e66265407a2b6834e63
vineepathak <pathakvinee@gmail.com>
2024-03-16 17:54:30 +0530

    Update Header.tsx
    Header

--- src/sxastarter/src/components/Header/Header.tsx
+++ src/sxastarter/src/components/Header/Header.tsx
@@ -37,21 +37,34 @@ export const Default = (props: HeaderProps): JSX.Element => {
       //   <div className={`component Header ${props.params.styles}`} id={id ? id : undefined}>
       <div className={`component Header ${props?.params?.styles}`}>
         <div className="component-content">
-          <JssLink field={props.fields.logoLink}>
-            <div className="field-Headericon">
-              <JssImage field={props.fields.logo} />
-            </div>
-          </JssLink>
-          {/* <div className="Header-text">
-            <div>
-              <div className="field-Headertext">
-                <JssRichText field={props.fields.HeaderText} />
+          <div className="w-full px-6 py-5 z-50 fixed top-0 shadow-md transition-all transform ease-in-out duration-500">
+            <div className="max-w-7xl mx-auto flex items-center flex-wrap justify-between">
+              <div className="sm:mr-8">
+                <JssLink field={props.fields.logoLink}>
+                  <JssImage
+                    width="100px"
+                    field={props.fields.logo}
+                    className="text-xl text-teal-700 font-semibold self-center"
+                  />
+                </JssLink>
               </div>
+              <nav
+                id="menu"
+                className="order-last md:order-none items-center flex-grow w-full md:w-auto md:flex hidden mt-2 md:mt-0"
+              >
+                {props.fields.generalLinks.map((items, index) => (
+                  <JssLink
+                    className="block mt-4 md:inline-block md:mt-0 font-medium text-slate-700 hover:text-teal-600 text-3xl mr-4"
+                    field={items.fields.link}
+                  />
+                ))}
+              </nav>
+              <form id="search" action="{{ '/search' | url }}" className="order-last sm:order-none flex-grow items-center justify-end hidden sm:block mt-6 sm:mt-0">
+                <label className="visually-hidden" htmlFor="header-searchbox">Search here ...</label>
+                <input type="text" id="header-searchbox" name="q" placeholder="Search here ..." className="w-full sm:max-w-xs bg-slate-200 border border-transparent float-right focus:bg-white focus:border-slate-300 focus:outline-none h-8 p-4 placeholder-slate-500 rounded text-slate-700 text-sm"/>
+            </form>
             </div>
-            <div className="field-Headerlink">
-              <JssLink field={props.fields.HeaderLink} />
-            </div>
-          </div>*/}
+          </div>
         </div>
       </div>
     );
