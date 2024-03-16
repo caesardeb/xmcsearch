bce318d7da808694a80e2e6825ec436355d60bcf
Shantipriya Adhau R <sadhau@horizontal.com>
2024-03-17 04:48:43 +0530

    Added search query

--- src/sxastarter/src/components/Header/Header.tsx
+++ src/sxastarter/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Image as JssImage,
   Link as JssLink,
   ImageField,
   LinkField,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import { useRouter } from 'next/router';
 
 interface Fields {
   logo: ImageField;
@@ -32,6 +33,21 @@ const HeaderDefaultComponent = (props: HeaderProps): JSX.Element => (
 
 export const Default = (props: HeaderProps): JSX.Element => {
     const [isToggleClicked, setIsToggleClicked] = useState(false);
+
+    const searchInputRef = useRef('');
+
+    const router = useRouter();
+
+    const handleSearch = () => {
+        const searchQuery = searchInputRef?.current?.value;
+
+        router.push({
+            pathname: '/search',
+            query: {
+                q: searchQuery,
+            },
+        });
+    };
   // const id = props.params.RenderingIdentifier;
   if (props.fields) {
     return (
@@ -74,8 +90,9 @@ export const Default = (props: HeaderProps): JSX.Element => {
                   name="q"
                   placeholder="Search here ..."
                   className="w-full sm:max-w-2xl bg-slate-200 border border-transparent float-right focus:bg-white focus:border-slate-300 focus:outline-none h-16 p-4 placeholder-slate-500 rounded text-slate-700 text-xl"
+                  ref={searchInputRef}
                 />
-                <svg className="absolute right-2 cursor-pointer top-1" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z"/></svg>
+                <svg className="absolute right-2 cursor-pointer top-1" onClick={() => handleSearch()} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z"/></svg>
               </form>
               <div
                 id="menu-toggle"
