2a04a74e5af8687b6426023dc76845207c033746
caesardeb <debtanu.b@horizontal.com>
2024-03-16 20:14:51 +0530

    Fix

--- src/sxastarter/src/components/Header/Header.tsx
+++ src/sxastarter/src/components/Header/Header.tsx
@@ -37,7 +37,7 @@ export const Default = (props: HeaderProps): JSX.Element => {
       //   <div className={`component Header ${props.params.styles}`} id={id ? id : undefined}>
       <div className={`component Header ${props?.params?.styles}`}>
         <div className="component-content">
-        <div className="bg-white w-full px-6 py-5">
+          <div className="bg-white w-full px-6 py-5">
             <div className="max-w-7xl mx-auto flex items-center flex-wrap justify-between">
               <div className="sm:mr-8">
                 <JssLink field={props.fields.logoLink}>
@@ -53,19 +53,49 @@ export const Default = (props: HeaderProps): JSX.Element => {
                 className="order-last md:order-none items-center flex-grow w-full md:w-auto md:flex hidden mt-2 md:mt-0"
               >
                 {props.fields.generalLinks.map((items, index) => (
-                  <JssLink
-                    className="block mt-4 md:inline-block md:mt-0 font-medium text-slate-700 hover:text-teal-600 text- mr-4"
-                    field={items.fields.link}
-                  />
+                  <div key={index}>
+                    <JssLink
+                      className="block mt-4 md:inline-block md:mt-0 font-medium text-slate-700 hover:text-teal-600 text- mr-4"
+                      field={items.fields.link}
+                    />
+                  </div>
                 ))}
               </nav>
-              <form id="search" action="{{ '/search' | url }}" className="order-last sm:order-none flex-grow items-center justify-end hidden sm:block mt-6 sm:mt-0">
+              <form
+                id="search"
+                action="{{ '/search' | url }}"
+                className="order-last sm:order-none flex-grow items-center justify-end hidden sm:block mt-6 sm:mt-0"
+              >
                 {/* <label className="visually-hidden" htmlFor="header-searchbox">Search here ...</label> */}
-                <input type="text" id="header-searchbox" name="q" placeholder="Search here ..." className="w-full sm:max-w-sm bg-slate-200 border border-transparent float-right focus:bg-white focus:border-slate-300 focus:outline-none h-12 p-4 placeholder-slate-500 rounded text-slate-700 text-xl"/>
-            </form>
-            <div id="menu-toggle" className="flex items-center md:hidden text-slate-700 hover:text-teal-600 cursor-pointer sm:ml-6">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-            </div>
+                <input
+                  type="text"
+                  id="header-searchbox"
+                  name="q"
+                  placeholder="Search here ..."
+                  className="w-full sm:max-w-sm bg-slate-200 border border-transparent float-right focus:bg-white focus:border-slate-300 focus:outline-none h-12 p-4 placeholder-slate-500 rounded text-slate-700 text-xl"
+                />
+              </form>
+              <div
+                id="menu-toggle"
+                className="flex items-center md:hidden text-slate-700 hover:text-teal-600 cursor-pointer sm:ml-6"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-menu"
+                >
+                  <line x1="3" y1="12" x2="21" y2="12"></line>
+                  <line x1="3" y1="6" x2="21" y2="6"></line>
+                  <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
