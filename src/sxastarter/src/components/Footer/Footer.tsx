3565244f732ae2130f8fb24568052b4926ced27e
caesardeb <debtanu.b@horizontal.com>
2024-03-16 23:40:36 +0530

    Linting issues

--- src/sxastarter/src/components/Footer/Footer.tsx
+++ src/sxastarter/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import {
   Link as JssLink,
   ImageField,
   LinkField,
-  RichText as JssRichText
+  RichText as JssRichText,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
@@ -35,7 +35,9 @@ const FooterDefaultComponent = (props: FooterProps): JSX.Element => (
 
 export const Default = (props: FooterProps): JSX.Element => {
   if (props.fields) {
-    {console.log(props)}
+    {
+      console.log(props);
+    }
     return (
       <div className={`p-0 component Footer ${props?.params?.styles}`}>
         <div className="component-content">
@@ -50,24 +52,24 @@ export const Default = (props: FooterProps): JSX.Element => {
                   />
                 </JssLink>
               </div>
-              <div className='order-last md:order-none items-center text-center flex-grow w-full md:w-3/4 md:flex mt-2 md:mt-0'>
+              <div className="order-last md:order-none items-center text-center flex-grow w-full md:w-3/4 md:flex mt-2 md:mt-0">
                 {props.fields.generalLink?.map((items, index) => (
-                    < >
+                  <>
                     <JssLink
-                    key={index}
-                        className="list-disc block mt-4 md:inline-block md:mt-0 text-2xl font-medium text-slate-700 hover:text-cyan-500 text- mr-4"
-                        field={items.fields.link}
+                      key={index}
+                      className="list-disc block mt-4 md:inline-block md:mt-0 text-2xl font-medium text-slate-700 hover:text-cyan-500 text- mr-4"
+                      field={items.fields.link}
                     />
-                    </>
+                  </>
                 ))}
               </div>
             </div>
-            <div className='max-w-7xl mx-auto items-center justify-between'>
-                <div className="mt-5 px-10 py-8">
-                    <div className="max-w-5xl mx-auto text-slate-700 text-center">
-                    <JssRichText field={props.fields.copyrightText} /> 
-                    </div>
+            <div className="max-w-7xl mx-auto items-center justify-between">
+              <div className="mt-5 px-10 py-8">
+                <div className="max-w-5xl mx-auto text-slate-700 text-center">
+                  <JssRichText field={props.fields.copyrightText} />
                 </div>
+              </div>
             </div>
           </div>
         </div>
